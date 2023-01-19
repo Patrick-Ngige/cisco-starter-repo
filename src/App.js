f386a4a86a0eb474badeb055e46313b965824e08
Patrick-Ngige <wakemanjajr@gmail.com>
2023-01-20 00:00:45 +0300

    clean code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
 import MainBanner from "./components/banners/MainBanner";
 import Wrap from "./components/wrap";
 
-function App() {
-  return (
-    <div >
-      < MainBanner />
-      < Wrap />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+          <MainBanner MainBannerText='Sextant' />
+          <Wrap name="This is the first exhibition!"></Wrap>
+          <Wrap name="Here is the second exhibition!"></Wrap> 
+          <Wrap name="This is the final exhibition!"></Wrap>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div >
+//       < MainBanner />
+//       < Wrap />
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/banners/MainBanner.js
+++ src/components/banners/MainBanner.js
@@ -1,12 +1,26 @@
-import volunteer from '../../images/volunteer.png';
+import React, { Component } from 'react';
+import './banner.css';
 
-function   MainBanner() {
-  return (
-      <div className='container w-100 bg-gray-200  text-gray-800  transition ease-in-out text-center'>
-          <p className="text-[70px] font-sans-[100px] py-2">Sextant</p>
-          <img src={volunteer} alt='Main banner'className='w-[100%] h-[400px] '/>
-    </div>
-  );
-};
+// import volunteer from '../../images/volunteer.png';
+
+class MainBanner extends Component {
+  render () {
+    return (
+      <div className='MainBanner'>
+        <h1 >{this.props.MainBannerText}</h1>
+      </div>
+    );
+  }
+}
+
+// class 
+// function   MainBanner() {
+//   return (
+//       <div className='container w-100 bg-gray-200  text-gray-800  transition ease-in-out text-center'>
+//           <p className="text-[70px] font-sans-[100px] py-2">Sextant</p>
+//           <img src={volunteer} alt='Main banner'className='w-[100%] h-[400px] '/>
+//     </div>
+//   );
+// };
 
 export default MainBanner
