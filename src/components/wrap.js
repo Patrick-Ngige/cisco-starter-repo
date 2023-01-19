a20c6e0d1de182102d5bbbd07f9bf97bdb9ce95f
Patrick-Ngige <wakemanjajr@gmail.com>
2023-01-20 00:00:10 +0300

    few corrections on components and exhibition

--- src/components/wrap.js
+++ src/components/wrap.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { Component } from "react";
+import "./wrap.css";
 
-function wrap() {
-  return (
-    <div className='w-100 h-60 bg-green-50 text-center text-[60px]'>wrap</div>
-  )
+class wrap extends Component {
+  render() {
+    return (
+      <div className="wrap">
+        <h2 className="wrapHeading">{this.props.name}</h2>
+        <div className="wrapContent">{this.props.children}</div>
+      </div>
+    );
+  }
 }
+// function wrap() {
+//   return (
+//     <div className='w-100 h-60 bg-green-50 text-center text-[60px]'>wrap</div>
+//   )
+// }
 
-export default wrap
+export default wrap;
