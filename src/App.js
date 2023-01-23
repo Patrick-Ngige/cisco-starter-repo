fb789eb4b418f030397b58230ad97a0416b980a8
Patrick-Ngige <wakemanjajr@gmail.com>
2023-01-24 01:13:46 +0300

    corrections

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import MainBanner from "./components/banners/MainBanner";
 import Wrap from "./components/wrap";
-
-
-
+import AddressDisplay from './AddressDisplay';
 
 
 class App extends Component {
   render() {
-
-    
-
     return (
       <div className='App'>
           <MainBanner MainBannerText='Sextant' />
-          <Wrap name="This is the first exhibition!"></Wrap>
-          <Wrap name="Here is the second exhibition!"></Wrap> 
-          <Wrap name="This is the final exhibition!"></Wrap>
+          <Wrap name="Public IPv4 Address">
+            <AddressDisplay url='https://api.ipify.org?format=json' />
+          </Wrap>
+          <Wrap name="Public IPv6 Address">
+          <AddressDisplay url='https://api64.ipify.org?format=json' />
+          </Wrap> 
+      
       </div>
     );
   }
